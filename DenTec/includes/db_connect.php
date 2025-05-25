2e04008599fb7de6c1bfd52e869c24e950f23906
Kosala1988 <kosalamuthukumarana@gmail.com>
2025-05-25 14:29:50 +0530

    Update db_connect.php

--- DenTec/includes/db_connect.php
+++ DenTec/includes/db_connect.php
@@ -1,12 +1,12 @@
 <?php
 // Database connection file
 
-$host = 'localhost';
-$db   = 'dental_management';
-$user = 'root';
-$pass = '';
+$host = $_ENV['MYSQLHOST'] ?? 'mysql.railway.internal';
+$user = $_ENV['MYSQLUSER'] ?? 'root';
+$pass = $_ENV['MYSQLPASSWORD'] ?? 'tpUIlkAzENJxUIPmnBvHXQumimrQyysD';
+$dbname = $_ENV['MYSQLDATABASE'] ?? 'railway';
 
-$conn = new mysqli($host, $user, $pass, $db);
+$conn = new mysqli($host, $user, $pass, $dbname);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
